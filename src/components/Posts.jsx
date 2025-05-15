aa0ec284c1c1cc7bd600bdb7d0d2cfbb40317a2f
alexander <alexander.t.simonsen@gmail.com>
2025-05-15 09:38:08 +0200

    kör

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -25,6 +25,13 @@ function Posts() {
           {/* Längre text för att trigga scroll i postCard */}
         </p>
       </div>
+      <div className={styles.postCard}>
+        <h3 className={styles.postCardTitle}>Inlägg 1</h3>
+        <p className={styles.postCardText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+          {/* Längre text för att trigga scroll i postCard */}
+        </p>
+      </div>
       
     </section>
   );
