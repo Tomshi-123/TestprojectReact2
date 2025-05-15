844cf7458359e24ba6e22131caea608ff6148123
alexander <alexander.t.simonsen@gmail.com>
2025-05-15 09:56:22 +0200

    simonsen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.customFooter}>
-      <p className={styles.footerText}>© 2025 Mitt Företag. Alla rättigheter förbehållna.</p>
+      <p className={styles.footerText}>© 2025 Simonsen. Alla rättigheter förbehållna.</p>
     </footer>
   );
 }
