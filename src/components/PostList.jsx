e50dc4f93e687b7951f9bc2b808b50f2e2b320d2
alexander <alexander.t.simonsen@gmail.com>
2025-05-21 10:09:49 +0200

    Retro2

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,16 +4,16 @@ import Post from './Posts';
 function PostList() {
   const posts = [
     {
-      title: 'This is The Post Title 1',
-      text: 'The Post Body: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     },
     {
-      title: 'This is The Post Title 2',
-      text: 'The Post Body: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      title: 'Title',
+      text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
-      title: 'This is The Post Title 3',
-      text: 'The Post Body: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      title: 'Title',
+      text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     }
   ];
 
